e486e5a30dbb7b1f1ed1dbadb77ec97f38183b77
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-04-18 16:04:14 -0300

    Requisito 1 - ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import StarwarsContext from './Context/StarwarsContext';
+import fetchPlanets from './Services/planetsApi';
+import Table from './Components/Table';
 
 function App() {
+  const [data, setPlanets] = useState();
+
+  async function getPlanets() {
+    const planets = await fetchPlanets();
+    setPlanets(planets);
+  }
+
+  getPlanets();
+
   return (
-    <span>Hello, App!!</span>
-    // inicio
+    <StarwarsContext.Provider value={ data }>
+      <Table />
+    </StarwarsContext.Provider>
   );
 }
 
